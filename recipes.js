697c2620734d6755c2c7cc89482fc8a028c90764
guilhermeaeme <guilhermeaeme@gmail.com>
2019-01-24 22:06:18 -0200

    interaction 1,2 and 3

--- recipes.js
+++ recipes.js
@@ -8,3 +8,28 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+  const receipeSchema = new Schema({
+  title: {type: String, required: true, unique: true },
+  level: {type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']},
+  ingredients: {type: Array},
+  cuisine: {type: String, required: true},
+  dishType: {type: String, enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']},
+  image: {type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg'},
+  duration: {type: Number, min: 0},
+  creator: {type: String},
+  created: {type: Date, default: Date.now}
+  });
+
+
+  const Recipe = mongoose.model('Recipe', receipeSchema);
+  
+  Recipe.create({ title: 'Hamburguer', level: 'Easy Peasy', ingredients: ['Pao', 'Carne', 'Alface', 'Tomate'], cuisine: 'American', dishType: 'Snack', duration: 10, creator: 'Diogo'})
+  .then(recipe => {console.log('the recipe is saved and its value is: ', recipe) }) 
+  .catch(err => {console.log('An error happened: ', err) });
+
+Recipe.insertMany( data, function(error, docs) {
+  docs.forEach(element => {
+    console.log(element.title)
+  });
+} );
